116b6000abbc23c3bdaa24ceaf57a8be7e683c9e
bajran <youraksh@gmail.com>
2020-06-30 12:02:49 +0530

    first Commit

---
+++ src/build/precache-manifest.6de45b7a107548577e288830f8e29265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cd349080e7b0fd38e2040a1aaca51d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8773fca4daa642b21e3",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "9412c9bac428443b84e3",
+    "url": "/static/js/2.364b4982.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.364b4982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8773fca4daa642b21e3",
+    "url": "/static/js/main.f21c4b43.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
